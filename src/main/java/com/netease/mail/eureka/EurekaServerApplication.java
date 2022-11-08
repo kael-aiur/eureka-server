d55ba91819fe7488a8051870d056dcb837c03e46
凌绝 <penghongjie@corp.netease.com>
2022-11-08 18:32:13 +0800

    fix

---
+++ src/main/java/com/netease/mail/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.netease.mail.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 凌绝 (penghongjie@corp.netease.com)
+ * @since ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
